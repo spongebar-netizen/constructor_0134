719686ce2da0c04ee9ca9eeef0ed20a50158a8c0
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-13 11:13:11 +0700

    void setID void printAll

--- staticfunction/staticfunction.cpp
+++ staticfunction/staticfunction.cpp
@@ -1,18 +1,47 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
 
 class mahasiswa {
 private:
-	static int nim;
+    static long int nim;
 public:
-	int id;
-	string nama;
+    long int id;
+    string nama;
 
-	void setID();
-	void printALL();
-	static void setNim(int pNim) { nim = pNim; }
-	static int getNim() { return nim; }
+    void setID();
+    void printAll();
 
-	mahasiswa(string pnama) : nama(pnama) { setID(); }
+    static void setNim(int pNim) { nim = pNim; }
+    static int getNim() { return nim; }
+    mahasiswa(string pnama) {
+        nama = pnama;
+        setID();
+    }
+
 };
+
+long int mahasiswa::nim = 20230140134;
+void mahasiswa::setID() {
+    id = ++nim;
+}
+void mahasiswa::printAll() {
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    mahasiswa mhs1("Lalu Idrak Yadafi Fatan Nuraga");
+    mahasiswa mhs2("Rafi Mino");
+    mahasiswa::setNim(9);
+    mahasiswa mhs3("Rifqi x bok");
+    mahasiswa mhs4("Dito yz");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "akses dari luar object = " << mahasiswa::getNim() << endl;
+}
