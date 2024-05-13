ca70e7cf387d7ac34cb8368838d088e576d9f2be
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-13 11:41:01 +0700

    void ceta

---
+++ construktor/construktor.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+	int nim;
+	string nama;
+
+public:
+	Mahasiswa()
+	{
+		nim = 20230140134;
+		nama = "Lalu Idrak Yadafi Fatan Nuraga";
+
+	};
+	Mahasiswa(int iNim) {
+		nim = iNim;
+	}
+	Mahasiswa(string iNama) {
+		nama = iNama;
+	}
+	Mahasiswa(int iNim, string iNama) {
+		nim = iNim;
+		nama = iNama;
+	}
+
+	void cetak() {
+		cout << "NIM =" << nim << endl;
+		cout << "Nama = " << nama << endl;
+		cout << endl;
+	}
+};
+
+int main() {
+	Mahasiswa mhs1("Lalu Idrak Yadafi Fatan Nuraga");
+	Mahasiswa mhs2("mikael");
+	Mahasiswa mhs3("bayam");
+	Mahasiswa mhs4("fikar");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+	return 0;
+}
