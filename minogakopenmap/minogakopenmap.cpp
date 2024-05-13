8c2ad45ae9fefb04fffcf4cf46912b87173ebd9c
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-13 10:42:50 +0700

    return; 0;

--- minogakopenmap/minogakopenmap.cpp
+++ minogakopenmap/minogakopenmap.cpp
@@ -10,5 +10,39 @@ public:
 	string nama;
 
 	void setID();
+	void printALL();
+	mahasiswa(string pnama)
+	{
+		nama = pnama;
+		setID();
 
+	}
 };
+
+int mahasiswa::nim = 10;
+
+void mahasiswa::setID()
+{
+	id = ++nim;
+}
+
+void mahasiswa::printALL()
+{
+	cout << "ID =" << id << endl;
+	cout << "Nama =" << nama << endl;
+	cout << endl;
+}
+
+int main()
+{
+	mahasiswa mhs1("barats kecil");
+	mahasiswa mhs2("mino gak open map");
+	mahasiswa mhs3("yz kalah lawan terizla");
+	mahasiswa mhs4("faramis telat ulti");
+
+	mhs1.printALL();
+	mhs2.printALL();
+	mhs3.printALL();
+	mhs4.printALL();
+	return; 0;
+}
