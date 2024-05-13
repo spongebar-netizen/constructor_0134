a71a4f4e51afb199f54ce227f3584f9dc666ef40
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-13 18:54:40 +0700

    int panjang

--- destructor/destructor.cpp
+++ destructor/destructor.cpp
@@ -1,3 +1,41 @@
 #include<iostream>
-#include<string>
 using namespace std;
+
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int i) {
+		panjang = i;
+		arr = new int[i];
+		isiData();
+	}
+	~angka() {
+		cout << endl;
+		cetakData();
+		delete[]arr;
+		cout << "Alamat Array Sudah Dilepaskan" << endl;
+
+	}
+
+	void cetakData() {
+		for (int i = 1; i <= panjang; i++) {
+			cout << i << " = " << arr[i] << endl;
+		}
+	}
+	void isiData() {
+		for (int i = 1; i <= panjang; i++) {
+			cout << i << " = ";
+			cin >> arr[i];
+		}
+	}
+
+};
+
+int main() {
+	angka belajarcpp(3);
+	angka* ptrBelajarcpp = new angka(5);
+	delete ptrBelajarcpp;
+	return 0;
+}
